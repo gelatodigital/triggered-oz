6468c16d596976c1d6338d0ad16af4c2dcd1aed1
gitpusha <gitpusha@gmail.com>
2019-10-26 18:27:46 +0200

    writing mint script using ethers.js

---
+++ encodings.js
@@ -0,0 +1,18 @@
+const Web3 = require("web3");
+const web3 = new Web3(Web3.givenProvider);
+
+module.exports = getEncodedActionKyberTradeParams;
+
+function getEncodedActionKyberTradeParams(
+  src,
+  dest,
+  srcAmount,
+  minConversionRate
+) {
+  let encodedActionParams = web3.eth.abi.encodeParameters(
+    ["address", "address", "uint256", "uint256"],
+    [src, dest, srcAmount, minConversionRate]
+  );
+
+  return encodedActionParams;
+}
